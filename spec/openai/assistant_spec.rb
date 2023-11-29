3de6c3ed29206e003b6b959ed2c6edacc4f94278
Hoang Dinh Son Duong <duonghds@Hoangs-MBP.mshome.net>
2023-11-29 15:35:38 +0700

    feat: update test with subject described_class

--- spec/openai/assistant_spec.rb
+++ spec/openai/assistant_spec.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/BlockLength
-
 RSpec.describe Openai::Assistant do
-  before do
-    @api_key = "mock_api"
-    @instance = Openai::Assistant.new(@api_key)
-  end
+  subject { described_class.new("mock_api") }
 
   describe ".create_assistant" do
     context "with valid parameters" do
@@ -14,7 +9,7 @@ RSpec.describe Openai::Assistant do
         model = "gpt-3.5-turbo"
         instructions = "You are a personal math tutor. When asked a question, write and run Ruby code to answer the question."
         expect(RestClient).to receive(:post).and_return(double(code: 200, body: '{"id": "assistant_id"}'))
-        assistant = @instance.create_assistant(model, instructions)
+        assistant = subject.create_assistant(model, instructions)
         expect(assistant.id).to eq "assistant_id"
       end
     end
@@ -24,7 +19,7 @@ RSpec.describe Openai::Assistant do
         model = "invalid_model"
         instructions = "You are a personal math tutor. When asked a question, write and run Ruby code to answer the question."
         expect(RestClient).to receive(:post).and_return(double(code: 404, body: '{"error": {"code": "model_not_found"}}'))
-        err_msg = @instance.create_assistant(model, instructions)
+        err_msg = subject.create_assistant(model, instructions)
         expect(err_msg).to eq "model_not_found"
       end
     end
@@ -34,7 +29,7 @@ RSpec.describe Openai::Assistant do
         model = "gpt-3.5-turbo"
         instructions = "You are a personal math tutor. When asked a question, write and run Ruby code to answer the question."
         expect(RestClient).to receive(:post).and_raise(RestClient::ExceptionWithResponse.new(double(code: 500, body: '{"error": {"code": "internal_server_error"}}')))
-        err_msg = @instance.create_assistant(model, instructions)
+        err_msg = subject.create_assistant(model, instructions)
         expect(err_msg).to eq "internal_server_error"
       end
     end
@@ -45,7 +40,7 @@ RSpec.describe Openai::Assistant do
       it "retrieves the assistant" do
         assistant_id = "assistant_id"
         expect(RestClient).to receive(:get).and_return(double(code: 200, body: '{"id": "assistant_id"}'))
-        assistant = @instance.retrieve_assistant(assistant_id)
+        assistant = subject.retrieve_assistant(assistant_id)
         expect(assistant.id).to eq "assistant_id"
       end
     end
@@ -54,7 +49,7 @@ RSpec.describe Openai::Assistant do
       it "raises an error" do
         assistant_id = "invalid_id"
         expect(RestClient).to receive(:get).and_return(double(code: 404, body: '{"error": {"code": "assistant_not_found"}}'))
-        err_msg = @instance.retrieve_assistant(assistant_id)
+        err_msg = subject.retrieve_assistant(assistant_id)
         expect(err_msg).to eq "assistant_not_found"
       end
     end
@@ -63,7 +58,7 @@ RSpec.describe Openai::Assistant do
       it "returns the error code" do
         assistant_id = "assistant_id"
         expect(RestClient).to receive(:get).and_raise(RestClient::ExceptionWithResponse.new(double(code: 500, body: '{"error": {"code": "internal_server_error"}}')))
-        err_msg = @instance.retrieve_assistant(assistant_id)
+        err_msg = subject.retrieve_assistant(assistant_id)
         expect(err_msg).to eq "internal_server_error"
       end
     end
@@ -74,7 +69,7 @@ RSpec.describe Openai::Assistant do
       it "deletes the assistant" do
         assistant_id = "assistant_id"
         expect(RestClient).to receive(:delete).and_return(double(code: 200, body: '{"deleted": true}'))
-        result = @instance.delete_assistant(assistant_id)
+        result = subject.delete_assistant(assistant_id)
         expect(result).to eq true
       end
     end
@@ -83,7 +78,7 @@ RSpec.describe Openai::Assistant do
       it "raises an error" do
         assistant_id = "invalid_id"
         expect(RestClient).to receive(:delete).and_return(double(code: 404, body: '{"error": {"code": "assistant_not_found"}}'))
-        err_msg = @instance.delete_assistant(assistant_id)
+        err_msg = subject.delete_assistant(assistant_id)
         expect(err_msg).to eq "assistant_not_found"
       end
     end
@@ -92,7 +87,7 @@ RSpec.describe Openai::Assistant do
       it "returns the error code" do
         assistant_id = "assistant_id"
         expect(RestClient).to receive(:delete).and_raise(RestClient::ExceptionWithResponse.new(double(code: 500, body: '{"error": {"code": "internal_server_error"}}')))
-        err_msg = @instance.delete_assistant(assistant_id)
+        err_msg = subject.delete_assistant(assistant_id)
         expect(err_msg).to eq "internal_server_error"
       end
     end
@@ -102,7 +97,7 @@ RSpec.describe Openai::Assistant do
     context "with valid parameters" do
       it "returns a list of assistants" do
         expect(RestClient).to receive(:get).and_return(double(code: 200, body: '{"data": [{"id": "assistant_id"}]}'))
-        assistants = @instance.list_assistant
+        assistants = subject.list_assistant
         expect(assistants.size).to be > 0
         expect(assistants[0]["id"]).to eq "assistant_id"
       end
@@ -111,10 +106,9 @@ RSpec.describe Openai::Assistant do
     context "when RestClient raises an exception" do
       it "returns the error code" do
         expect(RestClient).to receive(:get).and_raise(RestClient::ExceptionWithResponse.new(double(code: 500, body: '{"error": {"code": "internal_server_error"}}')))
-        err_msg = @instance.list_assistant
+        err_msg = subject.list_assistant
         expect(err_msg).to eq "internal_server_error"
       end
     end
   end
 end
-# rubocop:enable Metrics/BlockLength
--- spec/openai/assistant_vcr_spec.rb
+++ spec/openai/assistant_vcr_spec.rb
@@ -5,18 +5,15 @@
 require "vcr"
 
 RSpec.describe Openai::Assistant do
-  $assistant = nil
-  before do
-    @api_key = "any_api" # need to use real api first to generate the vcr_cassettes with real data
-    @instance = Openai::Assistant.new(@api_key)
-  end
+  $assistant = nil # keep the response after create for another method like retrieve, delete,...
+  subject { described_class.new("mock_api") } # need to use real api first to generate the vcr_cassettes with real data
   describe ".create_assistant" do
     context "with valid parameters" do
       it "creates an assistant" do
         VCR.use_cassette("create_assistant_valid") do
           model = "gpt-3.5-turbo"
           instructions = "You are a personal math tutor. When asked a question, write and run Ruby code to answer the question."
-          $assistant = @instance.create_assistant(model, instructions)
+          $assistant = subject.create_assistant(model, instructions)
           expect($assistant).to be_an_instance_of(Openai::AssistantObj)
           expect($assistant.id).to_not be_nil
         end
@@ -28,7 +25,7 @@ RSpec.describe Openai::Assistant do
         VCR.use_cassette("create_assistant_invalid") do
           model = "invalid_model"
           instructions = "You are a personal math tutor. When asked a question, write and run Ruby code to answer the question."
-          err_msg = @instance.create_assistant(model, instructions)
+          err_msg = subject.create_assistant(model, instructions)
           expect(err_msg).to eq "model_not_found"
         end
       end
@@ -40,7 +37,7 @@ RSpec.describe Openai::Assistant do
       it "retrieves the assistant" do
         VCR.use_cassette("cretrieve_assistant_valid") do
           assistant_id = $assistant.id
-          assistant = @instance.retrieve_assistant(assistant_id)
+          assistant = subject.retrieve_assistant(assistant_id)
           expect(assistant).to be_an_instance_of(Openai::AssistantObj)
           expect(assistant.id).to eq assistant_id
         end
@@ -51,7 +48,7 @@ RSpec.describe Openai::Assistant do
       it "raises an error" do
         VCR.use_cassette("retrieve_assistant_invalid") do
           assistant_id = "invalid_id"
-          err_msg = @instance.retrieve_assistant(assistant_id)
+          err_msg = subject.retrieve_assistant(assistant_id)
           expect(err_msg).to eq nil
         end
       end
@@ -62,7 +59,7 @@ RSpec.describe Openai::Assistant do
     context "with valid parameters" do
       it "returns a list of assistants" do
         VCR.use_cassette("list_assistant_valid") do
-          assistants = @instance.list_assistant
+          assistants = subject.list_assistant
           expect(assistants.size).to be > 0
           expect(assistants[0]["id"]).to eq $assistant.id
         end
@@ -75,7 +72,7 @@ RSpec.describe Openai::Assistant do
       it "deletes the assistant" do
         VCR.use_cassette("delete_assistant_valid") do
           assistant_id = $assistant.id
-          result = @instance.delete_assistant(assistant_id)
+          result = subject.delete_assistant(assistant_id)
           expect(result).to eq true
         end
       end
@@ -85,7 +82,7 @@ RSpec.describe Openai::Assistant do
       it "raises an error" do
         VCR.use_cassette("delete_assistant_invalid") do
           assistant_id = "invalid_id"
-          err_msg = @instance.delete_assistant(assistant_id)
+          err_msg = subject.delete_assistant(assistant_id)
           expect(err_msg).to eq nil
         end
       end
