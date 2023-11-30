c07dead31bb10eacadd5e11b84d7afadaee100fb
Hoang Dinh Son Duong <duonghds@Hoangs-MacBook-Pro.local>
2023-12-01 01:21:08 +0700

    chore: remove prefix variable

--- lib/http/http.rb
+++ lib/http/http.rb
@@ -6,25 +6,25 @@ require "json"
 module Http
   # An http client
   class Client
-    def call_post(_url, req_body, headers)
-      http = Net::HTTP.new(_url.host, _url.port)
+    def call_post(url, req_body, headers)
+      http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
-      request = Net::HTTP::Post.new(_url.path, headers)
+      request = Net::HTTP::Post.new(url.path, headers)
       request.body = req_body unless req_body.nil?
       http.request(request)
     end
 
-    def call_delete(_url, headers)
-      http = Net::HTTP.new(_url.host, _url.port)
+    def call_delete(url, headers)
+      http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
-      request = Net::HTTP::Delete.new(_url.path, headers)
+      request = Net::HTTP::Delete.new(url.path, headers)
       http.request(request)
     end
 
-    def call_get(_url, headers)
-      http = Net::HTTP.new(_url.host, _url.port)
+    def call_get(url, headers)
+      http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
-      request = Net::HTTP::Get.new(_url.path, headers)
+      request = Net::HTTP::Get.new(url.path, headers)
       http.request(request)
     end
   end
