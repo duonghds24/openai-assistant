864661d18f2b87648867a9dacb30c28cc023bfd4
Hoang Dinh Son Duong <duonghds@Hoangs-MBP.mshome.net>
2023-12-01 15:08:28 +0700

    chore: optional disable ssl http client

--- lib/http/http.rb
+++ lib/http/http.rb
@@ -6,24 +6,25 @@ require "json"
 module Http
   # An http client
   class Client
-    def call_post(url, req_body, headers)
+    # disable_ssl instead of ssl because almost the host is https
+    def call_post(url, req_body, headers, disable_ssl = false) 
       http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
+      http.use_ssl = disable_ssl ? false : true
       request = Net::HTTP::Post.new(url.path, headers)
       request.body = req_body unless req_body.nil?
       http.request(request)
     end
 
-    def call_delete(url, headers)
+    def call_delete(url, headers, disable_ssl = false)
       http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
+      http.use_ssl = disable_ssl ? false : true
       request = Net::HTTP::Delete.new(url.path, headers)
       http.request(request)
     end
 
-    def call_get(url, headers)
+    def call_get(url, headers, disable_ssl = false)
       http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
+      http.use_ssl = disable_ssl ? false : true
       request = Net::HTTP::Get.new(url.path, headers)
       http.request(request)
     end
